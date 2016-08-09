e07a71f3959f79629e4476f05c33f9eb5aedc05b
xautanyang <xautanyang@163.com>
2016-08-09 16:07:16 +0800

    create wishcard table

--- conf/evolutions/card.sql
+++ conf/evolutions/card.sql
@@ -1,7 +1,9 @@
-create table if not exists card (
-    id int not null AUTO_INCREMENT primary key,
-    name varchar(20) not null,
-    summary varchar(100),
-    thumbnail varchar(100),
-    type int default 0
-);
+CREATE TABLE IF NOT EXISTS `card` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `summary` varchar(100) DEFAULT NULL,
+  `thumbnail` varchar(100) DEFAULT NULL,
+  `type` int(11) DEFAULT '0',
+  `score` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+)
