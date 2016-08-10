b0b9e4a576e051a9224731cb4a15e397b124a73b
xautanyang <xautanyang@163.com>
2016-08-10 09:09:47 +0800

    add person test data

--- conf/evolutions/test_data.sql
+++ conf/evolutions/test_data.sql
@@ -1,3 +1,11 @@
 delete from card;
 
-insert into card values(0, "name01", "summary", "thumbnail", 0, 0);
+INSERT INTO `card` (`id`, `name`, `summary`, `thumbnail`, `type`, `score`) VALUES
+	(1, 'name01', 'summary1', 'thumbnail1', 1, 1),
+	(2, 'name02', 'summary2', 'thumbnail2', 2, 2);
+
+delete from person;
+
+INSERT INTO `person` (`id`, `account`, `password`, `level`, `avatar`) VALUES
+	(1, '111', '111', 1, '1'),
+	(2, '222', '222', 2, '2');
